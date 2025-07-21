c9b01dc7cc343a1d040ac494b452057f08ef1378
Matías Boyer <matiasboyer7@hotmail.com>
2025-07-21 11:10:07 -0300

    main + api

---
+++ back_src/api/api.js
@@ -0,0 +1,42 @@
+import { Router } from "express";
+import db from "./db.js";
+
+const router = Router();
+
+let cache = {
+  data: {},
+  timestamp: 0,
+  next_fetch: 0,
+  countdown: 0,
+};
+
+let cache_time = 60 * 15;
+
+router.get("/get_routes", async (req, res) => {
+  const pool = await db.GetPool();
+  if (!pool) {
+    res.status(500).send({ message: ["no pool recvd"] });
+    return;
+  }
+
+  const conn = await db.GetConnection(pool);
+  if (!conn) {
+    res.status(500).send({ message: ["no conn recvd"] });
+    return;
+  }
+
+  const unix_time = new Date().getTime() / 1000;
+  cache.countdown = cache.next_fetch - unix_time;
+  if (unix_time >= cache.next_fetch) {
+    cache.data = await db.ExecQuery(
+      conn,
+      "SELECT label, iconsvg, url FROM routes"
+    );
+    cache.timestamp = unix_time;
+    cache.next_fetch = unix_time + cache_time;
+  }
+
+  res.status(200).send(cache);
+});
+
+export default router;
--- back_src/main.js
+++ back_src/main.js
@@ -2,8 +2,14 @@ import express from "express";
 import { createServer as createViteServer } from "vite";
 import path from "node:path";
 import { fileURLToPath } from "node:url";
+import fs from "node:fs/promises";
+import router from "./api/api.js";
 
 (async () => {
+  const __dirname = path.dirname(fileURLToPath(import.meta.url));
+  const indexHtmlPath = path.resolve(__dirname, "..", "index.html");
+  const rawHtml = await fs.readFile(indexHtmlPath, "utf-8");
+
   const app = express();
 
   const vite = await createViteServer({
@@ -13,16 +19,11 @@ import { fileURLToPath } from "node:url";
 
   app.use(vite.middlewares);
 
-  app.get("/api/hello", (req, res) => {
-    res.send("Hello!");
-  });
+  app.use("/api", router);
 
   app.use(/.*/, async (req, res, next) => {
     try {
-      const __dirname = path.dirname(fileURLToPath(import.meta.url));
-      const indexHtmlPath = path.resolve(__dirname, "..", "index.html");
-
-      let html = await vite.transformIndexHtml(req.url, indexHtmlPath);
+      const html = await vite.transformIndexHtml(req.url, rawHtml);
       res.status(200).set({ "Content-Type": "text/html" }).end(html);
     } catch (e) {
       vite.ssrFixStacktrace(e);

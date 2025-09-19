a26680ce2e1009f4c997f4b0de6ce1c3e6214e27
Matías Boyer <matiasboyer7@hotmail.com>
2025-09-19 15:43:50 -0300

    feat: viewport

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,10 +1,12 @@
 import "../styles/globals.css";
 import { metadata as c_metadata } from "../constants/metadata.constant";
+import { viewport as c_viewport } from "../constants/viewport.constant";
 import Footer from "@/components/footer.component";
 import { GeistSans } from "geist/font/sans";
 import { GeistMono } from "geist/font/mono";
 
 export const metadata = c_metadata;
+export const viewport = c_viewport;
 
 export default function RootLayout({
   children,
@@ -16,7 +18,7 @@ export default function RootLayout({
       <body
         className={`${GeistSans.variable} ${GeistMono.variable} antialiased min-h-screen flex flex-col`}
       >
-        <div className="flex-grow flex flex-col justify-center items-center">
+        <div className="flex-grow flex flex-col items-center m-10">
           {children}
         </div>
 

0fed918e19bae5d4e49685da9f7996823e487e0b
Matías Boyer <matiasboyer7@hotmail.com>
2025-09-19 12:12:55 -0300

    feat: basic inmem cache for geturl endpoint

--- backend/src/controllers/geturl.controller.ts
+++ backend/src/controllers/geturl.controller.ts
@@ -3,15 +3,43 @@ import service from "../services/geturl.service";
 import { IResponse } from "interfaces/iresponse.type";
 import { IGetUrl } from "interfaces/geturl.type";
 
+const cache_time = 15 * 60 * 1000;
+
+let cache: {
+  last_fetch: number;
+  data: IGetUrl[] | undefined;
+  pending?: Promise<IGetUrl[]>;
+} = {
+  last_fetch: 0,
+  data: undefined,
+};
+
 async function get(
   req: Request,
   res: Response,
   next: NextFunction
 ): Promise<void> {
   try {
+    if (cache.last_fetch + cache_time <= Date.now()) {
+      if (!cache.pending) {
+        cache.pending = service.get(req).then((data) => {
+          cache.data = data;
+          cache.last_fetch = Date.now();
+          cache.pending = undefined;
+          return data;
+        });
+      }
+
+      await cache.pending;
+    }
+
+    if (!cache.data) {
+      throw new Error("Failed to fetch data");
+    }
+
     const result: IResponse<IGetUrl[]> = {
       success: true,
-      data: await service.get(req),
+      data: cache.data,
     };
     res.status(200).json(result);
   } catch (err) {

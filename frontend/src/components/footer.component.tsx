e993b709ec6e28df9396d8b73d71a65b4de12852
Matías Boyer <matiasboyer7@hotmail.com>
2025-09-19 12:27:26 -0300

    fix: footer not centered

--- frontend/src/components/footer.component.tsx
+++ frontend/src/components/footer.component.tsx
@@ -6,16 +6,19 @@ import config from "@/utils/config.util";
 export default function Footer() {
   return (
     <footer className="w-full p-4 text-xs flex flex-row justify-between items-center">
-      <span>
+      <span className="text-left flex-1">
         made with ♥<br />
         matías boyer
       </span>
-      <span>
-        <a href="https://github.com/MatiasBoyer/bigraiden.com">
-          <Image src={github_mark} alt="github-icon" width={24} height={24} />
-        </a>
-      </span>
-      <span className="text-right">
+
+      <a
+        href="https://github.com/MatiasBoyer/bigraiden.com"
+        className="flex-1 flex justify-center"
+      >
+        <Image src={github_mark} alt="github-icon" width={24} height={24} />
+      </a>
+
+      <span className="text-right flex-1">
         build {config.build}
         <br />
         {config.branch}
